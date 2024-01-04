5430b8dd0a979e51225549997faa7cd3db0d02b4
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2024-01-04 04:12:29 +0300

    improved setup.py

--- setup.py
+++ setup.py
@@ -3,79 +3,58 @@ import platform
 from setuptools import Extension, setup
 from Cython.Build import cythonize
 
-WITH_DYLIB = os.getenv("WITH_DYLIB", True)
-
-INCLUDE_DIRS = []
-LIBRARY_DIRS = []
-EXTRA_OBJECTS = []
-EXTRA_LINK_ARGS = ['-mmacosx-version-min=13.6']
-LIBRARIES = ["pthread"]
-
-if WITH_DYLIB:
-    LIBRARIES.append('faust.2')
-else:
-    EXTRA_OBJECTS.append('lib/libfaust.a')
-
+PLATFORM = platform.system()
 CWD = os.getcwd()
 LIB = os.path.join(CWD, 'lib')
-LIBRARY_DIRS.append(LIB)
-INCLUDE_DIRS.append(os.path.join(CWD, 'include'))
+INCLUDE = os.path.join(CWD, 'include')
+
+
+INCLUDE_DIRS = [INCLUDE]
+LIBRARY_DIRS = [LIB]
+EXTRA_OBJECTS = []
+EXTRA_LINK_ARGS = []
+LIBRARIES = ["faust.2", "pthread"]
+DEFINE_MACROS = [("INTERP_DSP", 1)]
 EXTRA_COMPILE_ARGS = ['-std=c++11']
+RTAUDIO_SRC = [
+    "include/rtaudio/RtAudio.cpp",
+    "include/rtaudio/rtaudio_c.cpp",
+]
 
-# add local rpath
-if platform.system() == 'Darwin' and WITH_DYLIB:
-    EXTRA_LINK_ARGS.append('-Wl,-rpath,'+LIB)
+# platform specific
+if PLATFORM == 'Darwin':
+    EXTRA_LINK_ARGS.append('-mmacosx-version-min=13.6')
+    DEFINE_MACROS.append(("__MACOSX_CORE__", None)) # rtaudio for macos
+    EXTRA_LINK_ARGS.append('-Wl,-rpath,'+LIB) # add local rpath
+    os.environ['LDFLAGS'] = " ".join([
+        "-framework CoreFoundation",
+        "-framework CoreAudio"
+    ])
+elif PLATFORM == 'Linux':
+    DEFINE_MACROS.append(("__LINUX_ALSA__", None))
 
-os.environ['LDFLAGS'] = " ".join([
-    "-framework CoreFoundation",
-    "-framework CoreAudio"
-])
+
+def mk_extension(name, source, define_macros=None):
+    return Extension(
+        name,
+        source,
+        define_macros=define_macros if define_macros else [],
+        include_dirs = INCLUDE_DIRS,
+        libraries = LIBRARIES,
+        library_dirs = LIBRARY_DIRS,
+        extra_objects = EXTRA_OBJECTS,
+        extra_compile_args = EXTRA_COMPILE_ARGS,
+        extra_link_args = EXTRA_LINK_ARGS,
+    )
 
 extensions = [
-    Extension("cyfaust.interp", 
-        [
-            "src/cyfaust/interp.pyx", 
-            "include/rtaudio/RtAudio.cpp",
-            "include/rtaudio/rtaudio_c.cpp",
-        ],
-        define_macros = [
-            ("INTERP_DSP", 1),
-            ("__MACOSX_CORE__", None)
-        ],
-        include_dirs = INCLUDE_DIRS,
-        libraries = LIBRARIES,
-        library_dirs = LIBRARY_DIRS,
-        extra_objects = EXTRA_OBJECTS,
-        extra_compile_args = EXTRA_COMPILE_ARGS,
-        extra_link_args = EXTRA_LINK_ARGS,
+    mk_extension("cyfaust.interp", 
+        source=["src/cyfaust/interp.pyx"] + RTAUDIO_SRC,
+        define_macros=DEFINE_MACROS,
     ),
-    Extension("cyfaust.common", ["src/cyfaust/common.pyx"],
-        define_macros = [],
-        include_dirs = INCLUDE_DIRS,
-        libraries = LIBRARIES,
-        library_dirs = LIBRARY_DIRS,
-        extra_objects = EXTRA_OBJECTS,
-        extra_compile_args = EXTRA_COMPILE_ARGS,
-        extra_link_args = EXTRA_LINK_ARGS,
-    ),
-    Extension("cyfaust.signal", ["src/cyfaust/signal.pyx"],
-        define_macros = [],
-        include_dirs = INCLUDE_DIRS,
-        libraries = LIBRARIES,
-        library_dirs = LIBRARY_DIRS,
-        extra_objects = EXTRA_OBJECTS,
-        extra_compile_args = EXTRA_COMPILE_ARGS,
-        extra_link_args = EXTRA_LINK_ARGS,
-    ),
-    Extension("cyfaust.box", ["src/cyfaust/box.pyx"],
-        define_macros = [],
-        include_dirs = INCLUDE_DIRS,
-        libraries = LIBRARIES,
-        library_dirs = LIBRARY_DIRS,
-        extra_objects = EXTRA_OBJECTS,
-        extra_compile_args = EXTRA_COMPILE_ARGS,
-        extra_link_args = EXTRA_LINK_ARGS,
-    ),
+    mk_extension("cyfaust.common", ["src/cyfaust/common.pyx"]),
+    mk_extension("cyfaust.signal",["src/cyfaust/signal.pyx"]),
+    mk_extension("cyfaust.box", ["src/cyfaust/box.pyx"]),
 ]
 
 setup(
@@ -83,7 +62,7 @@ setup(
     version='0.0.1',
     ext_modules=cythonize(
         extensions,
-        language_level="3str",
+        language_level="3",
     ),
     package_dir = {"cyfaust": "src/cyfaust"},
     packages=['cyfaust']
